68c08c16fec36aed39c65980611dc0aa27b7efcf
Chris Gunkel <christopher.gunkel@centurylink.com>
2020-09-18 04:38:55 +0000

    updating string

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -18,7 +18,7 @@ new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 # When I ssh into IP addresses 10.0.0.1 or 10.20.30.1 I am unable to ping ports 5060, 80, or 55.
 print ("When I", new_list[5], "into the ip address", new_list[3], "or", new_list[4], "I am unable to ping ports", str(new_list[0]), ",", new_list[1], ", or", str(new_list[2]))
 
-#Concatenate
+# Concatenate Method for printing a line
 print(new_list)
 print(type(new_list[0]))
 print(type(new_list[1]))
@@ -26,11 +26,13 @@ print(type(new_list[2]))
 print(type(new_list[3]))
 print(type(new_list[4]))
 print(type(new_list[5]))
-#sentence_01 = "When I" + new_list[5] + "into IP addresses" + new_list[3] + "or," + new_list[4] + " I am unable to ping ports" + str(new_list[0] + "," + new_list[1] + "or," + str(new_list[2]
 sentence_01 = "When I " + new_list[5] + " into IP addresses " + new_list[3] + " or, " + new_list[4] + " I am unable to ping ports " + str(new_list[0]) + ", " + new_list[1] + ", or " + str(new_list[2])
 print(sentence_01)
 
-# .format()method
-sentence_03 = "When I {5} into IP address {3} or {4} I am unable to ping ports {0}, {1} or {2}." .format(*new_list)
+# .format() Method for printing a line
+sentence_03 = "When I {5} into IP addresses {3} or, {4} I am unable to ping ports {0}, {1}, or {2}." .format(*new_list)
 print(sentence_03)
 
+#f-string
+sentence_02 = f"When I {new_list[5]} into IP addresses {new_list[3]} or, {new_list[4]} I am unable to ping ports {new_list[0]}, {new_list[1]}, or {new_list[2]}."
+print(sentence_02)
